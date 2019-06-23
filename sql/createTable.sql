588fbf556bbb2b51421986db95b286d9f586ae4e
zhoulinhui <1047043261@qq.com>
2019-06-23 17:20:36 +0800

    添加了java类

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -9,6 +9,7 @@ create table Users
 );
 
 //hello zhoulinhui
+
 create table Questions
 ( question_id CHAR(5) PRIMARY KEY,
   user_id CHAR(8) UNIQUE ,
---
+++ target/classes/com/weblatter/entity/Workers.java
@@ -0,0 +1,58 @@
+public class Workers {
+    private String worker_id;
+    private String worker_name;
+    private int worker_age;
+    private String worker_phone_num;
+    private String id_numbers;
+    private String email;
+
+    public String getWorker_id() {
+        return worker_id;
+    }
+
+    public void setWorker_id(String worker_id) {
+        this.worker_id = worker_id;
+    }
+
+    public String getWorker_name() {
+        return worker_name;
+    }
+
+    public void setWorker_name(String worker_name) {
+        this.worker_name = worker_name;
+    }
+
+    public int getWorker_age() {
+        return worker_age;
+    }
+
+    public void setWorker_age(int worker_age) {
+        this.worker_age = worker_age;
+    }
+
+    public String getWorker_phone_num() {
+        return worker_phone_num;
+    }
+
+    public void setWorker_phone_num(String worker_phone_num) {
+        this.worker_phone_num = worker_phone_num;
+    }
+
+    public String getId_numbers() {
+        return id_numbers;
+    }
+
+    public void setId_numbers(String id_numbers) {
+        this.id_numbers = id_numbers;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
+
+

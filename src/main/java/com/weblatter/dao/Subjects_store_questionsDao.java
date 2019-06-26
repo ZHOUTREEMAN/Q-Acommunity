2a970184601cade25b88a4341f8a30f6353de96e
Diljane <13859994377@163.com>
2019-06-26 09:09:58 +0800

    dao by wj

--- src/main/java/com/weblatter/dao/Subjects_store_questionsDao.java
+++ src/main/java/com/weblatter/dao/Subjects_store_questionsDao.java
@@ -4,9 +4,24 @@ import com.weblatter.entity.SubjectsStoreQuestions;
 import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 
 @Repository
 @Mapper
-public interface Subjects_store_questions {
+public interface Subjects_store_questionsDao {
+    @Select("SELECT * FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subjectId}")
+    public SubjectsStoreQuestions selectSubject(String subjectId);
 
+    @Select("Select * FROM SUBJECTSSTOREQUESTIONS")
+    public List<SubjectsStoreQuestions> selectAllSubjects();
+
+    @Insert("INSERT INTO SUBJECTSSTOREQUESTIONS VALUES(#{subjectId}, #{questionId})")
+    public void insertSubjects(SubjectsStoreQuestions subjects);
+
+    @Delete("DELETE FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subjectId}")
+    public void deleteSubject(String subjectId);
+
+    @Update("UPDATE SUBJECTSSTOREQUESTIONS SET "+
+            "SUBJECT_ID=#{SUBJECT_ID},QUESTION_ID=#{QUESTION_ID}")
+    public void updateInformation(SubjectsStoreQuestions subjects);
 }

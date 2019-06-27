37cec2eccd6ebb958f9224e7da44e4cffb038e87
zhoulinhui <1047043261@qq.com>
2019-06-27 23:59:14 +0800

    周林辉

---
+++ src/main/java/com/weblatter/service/imple/QuestionService.java
@@ -0,0 +1,31 @@
+package com.weblatter.service.imple;
+
+import com.weblatter.dao.AnswersDao;
+import com.weblatter.dao.QuestionsDao;
+import com.weblatter.entity.Answers;
+import com.weblatter.entity.Questions;
+import com.weblatter.service.IQuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class QuestionService implements IQuestionService {
+    @Autowired
+    AnswersDao answersDao;
+    public Map<Questions, String> getHotQuestion(int questionNum) {
+        return null;
+    }
+
+    public List<Answers> getAnswers(String questionId, int num) {
+        List<Answers> answersList = answersDao.selectAnswer(questionId);
+        if (answersList != null) {
+            if (answersList.size() <= num)
+                return answersList;
+            else {
+                return answersList.subList(0, num - 1);
+            }
+        }else return null;
+    }
+}

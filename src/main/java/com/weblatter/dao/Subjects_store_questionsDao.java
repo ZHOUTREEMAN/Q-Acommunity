5467cd4e00b7641a7a7dd3123a4e1277fe019a61
zhoulinhui <1047043261@qq.com>
2019-06-27 08:38:52 +0800

    dao by zhou

--- src/main/java/com/weblatter/dao/Subjects_store_questionsDao.java
+++ src/main/java/com/weblatter/dao/Subjects_store_questionsDao.java
@@ -9,19 +9,19 @@ import java.util.List;
 @Repository
 @Mapper
 public interface Subjects_store_questionsDao {
-    @Select("SELECT * FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subjectId}")
+    @Select("SELECT * FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subject_id}")
     public SubjectsStoreQuestions selectSubject(String subjectId);
 
     @Select("Select * FROM SUBJECTSSTOREQUESTIONS")
     public List<SubjectsStoreQuestions> selectAllSubjects();
 
-    @Insert("INSERT INTO SUBJECTSSTOREQUESTIONS VALUES(#{subjectId}, #{questionId})")
+    @Insert("INSERT INTO SUBJECTSSTOREQUESTIONS VALUES(#{subject_id}, #{question_id})")
     public void insertSubjects(SubjectsStoreQuestions subjects);
 
-    @Delete("DELETE FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subjectId}")
+    @Delete("DELETE FROM SUBJECTSSTOREQUESTIONS WHERE SUBJECT_ID=#{subject_id}")
     public void deleteSubject(String subjectId);
 
     @Update("UPDATE SUBJECTSSTOREQUESTIONS SET "+
-            "SUBJECT_ID=#{SUBJECT_ID},QUESTION_ID=#{QUESTION_ID}")
+            "SUBJECT_ID=#{subject_id},QUESTION_ID=#{question_id}")
     public void updateInformation(SubjectsStoreQuestions subjects);
 }

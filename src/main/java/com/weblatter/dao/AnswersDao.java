a5333095931709aa5ed40616e8c998c00d02a9a9
zhoulinhui <1047043261@qq.com>
2019-06-27 21:28:27 +0800

    Dao update by zhoulinhui

--- src/main/java/com/weblatter/dao/AnswersDao.java
+++ src/main/java/com/weblatter/dao/AnswersDao.java
@@ -10,21 +10,21 @@ import java.util.List;
 @Repository
 @Mapper
 public interface AnswersDao {
-    @Select("SELECT * FROM ANSWERS WHERE question_id=#{question_id}")
-    public List<Answers> selectAnswer(String question_id);
+    @Select("SELECT * FROM ANSWERS WHERE answer_id=#{answer_id}")
+    public Answers selectAnswer(String answer_id);
 
     @Select("Select * FROM ANSWERS")
     public List<Answers> selectAllAnswers();
 
     @Insert("INSERT INTO ANSWERS VALUES(#{answer_id}, #{answer_user}, #{answer_time}, #{support_num}, #{objection_num}," +
-            " #{question_id}, #{answer})")
+            " #{question_id}, #{answer},#{processing_status},#{processing_id})")
     public void insertQuestions(Answers answers);
 
     @Delete("DELETE FROM ANSWERS WHERE ANSWER_ID=#{answer_id}")
     public void deleteAnswers(String answer_id);
 
-    @Update("UPDATE ANSWERS SET ANSWER_USER=#{answer_user}," +
+    @Update("UPDATE ANSWERS SET " +
             "ANSWER_TIME=#{answer_time},SUPPORT_NUM=#{support_num},OBJECTION_NUM=#{objection_num}," +
-            "ANSWER=#{answer} WHERE ANSWER_ID=#{answer_id} ")
+            "ANSWER=#{answer},PROCESSING_STATUS=#{processing_status},PROCESSING_ID=#{processing_id} WHERE ANSWER_ID=#{answer_id} ")
     public void updateInformation(Answers answers);
 }
